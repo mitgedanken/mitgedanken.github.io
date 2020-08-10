104b532ab10645abb531d57f4fd82f442b16e851
Restyled.io <commits@restyled.io>
2020-08-10 22:10:24 +0000

    Restyled by clang-format

--- _webpushr.js
+++ _webpushr.js
@@ -1,3 +1,14 @@
-(function(w,d, s, id){const _key='BOjC7TMLPxN8fSV99FAy_Kj-jUQ7WtPAek-ie26397IryGd56rnUz15hlDqTJXzr1o0Hty8g9gIIMQOjXTHGesU';
-if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s);js.id = id;js.src = "https://cdn.webpushr.com/app.min.js";
-fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));webpushr('setup',{'key': _key});
+(function(w, d, s, id) {
+const _key =
+    'BOjC7TMLPxN8fSV99FAy_Kj-jUQ7WtPAek-ie26397IryGd56rnUz15hlDqTJXzr1o0Hty8g9gIIMQOjXTHGesU';
+if (typeof (w.webpushr) !== 'undefined')
+  return;
+w.webpushr = w.webpushr ||
+             function() { (w.webpushr.q = w.webpushr.q || []).push(arguments) };
+var js, fjs = d.getElementsByTagName(s)[0];
+js = d.createElement(s);
+js.id = id;
+js.src = "https://cdn.webpushr.com/app.min.js";
+fjs.parentNode.appendChild(js);
+}(window, document, 'script', 'webpushr-jssdk'));
+webpushr('setup', {'key' : _key});
